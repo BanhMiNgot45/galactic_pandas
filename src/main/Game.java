ccfc8d5be041e349aaeb5faf879043be5a97a7a4
Steven Le <stevenhle459@gmail.com>
2022-01-12 22:41:14 -0600

    Init commit

---
+++ src/main/Game.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Game {
+	private long window;
+	
+	public void run() {
+		
+	}
+	
+	private void init() {
+		
+	}
+	
+	private void loop() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game().run();
+	}
+}
